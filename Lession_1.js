2080f00d45662cb01087dc79625aca11d785fac7
Nilesh <nileshk191@gmail.com>
2022-03-21 10:33:39 +0530

    javascript

---
+++ Lession_1.js
@@ -0,0 +1,65 @@
+
+
+function add() {
+    // let a = 15
+    // let b = 10
+
+    // c = a+b
+    // // alert(c)
+    // console.log(c)
+
+
+// Number
+// String
+// Object
+// null
+// undefined
+// {
+    // for()
+    // let a =10
+
+
+// }
+
+// var or let
+var a = 10
+let b = 10
+var c = a+b
+console.log("This is numbers adding",c)
+
+var name = "nilesh"
+let name1 = "nilesh"
+var string = name+name1
+console.log("This is adding string",string)
+const pi = 3.14
+
+const company = "heliophiles"
+
+let person = {
+    "name":"sunil Kadu",
+    "dept":"Mechanical",
+    "Marks": 37
+}
+console.log(person)
+
+
+
+
+
+}
+// var a = 10
+
+function getcity(){
+    console.log(a)
+    var a = document.getElementById('first').value
+
+
+    console.log("value of A",a)
+}
+
+// console.log(a)
+
+function getinputvalue(){
+    let a = document.getElementsByClassName("inputbox")
+    console.log(a)
+}
